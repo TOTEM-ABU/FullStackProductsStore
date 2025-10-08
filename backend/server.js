5e05984b007a1ddd8a6b18cd6eac1d73b0cec512
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-10-08 09:04:48 +0300

    Coupon routes done

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
+import couponsRoutes from "./routes/coupons.routes.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponsRoutes);
 
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:" + PORT);
